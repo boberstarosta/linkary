1a316bf242b5d9de77c0b05eedda07139a1b26f4
boberstarosta <boberstarosta@gmail.com>
2018-02-11 00:02:13 +0100

    Set up default login/logout redirect

--- website/settings.py
+++ website/settings.py
@@ -96,6 +96,10 @@ AUTH_PASSWORD_VALIDATORS = [
 ]
 
 
+# Login redirect settings
+LOGIN_REDIRECT_URL = 'index'
+LOGOUT_REDIRECT_URL = 'index'
+
 # Internationalization
 # https://docs.djangoproject.com/en/2.0/topics/i18n/
 
