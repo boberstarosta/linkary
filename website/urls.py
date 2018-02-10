55a98f2a39ed8c7a51aa37379a3243189a43e388
boberstarosta <boberstarosta@gmail.com>
2018-02-10 23:06:00 +0100

    dded accounts url (login)

--- website/urls.py
+++ website/urls.py
@@ -1,9 +1,10 @@
 from django.contrib import admin
-from django.urls import path
+from django.urls import include, path
 from linkary import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('accounts/', include('django.contrib.auth.urls')),
     path('', views.LinkListView.as_view(), name='index'),
     path('link/<int:pk>/', views.LinkDetailView.as_view(), name='link_detail'),
     path('link/new/', views.LinkCreateView.as_view(), name='link_create'),
