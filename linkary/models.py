e7d8f095c2912145ae91bc44662a72d0fe6b5642
boberstarosta <boberstarosta@gmail.com>
2018-02-10 12:57:10 +0100

    Created Category and Link models

--- linkary/models.py
+++ linkary/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+# To use User as ForeignKey:
+# from django.contrib.auth.models import User
+# user = models.ForeignKey(User, on_delete=models.SET_NULL, blank=True, null=True)
 
 
+class Category(models.Model):
+    parent = models.ForeignKey('self', on_delete=models.CASCADE)
+    name = models.CharField(max_length=100)
+
+
+class Link(models.Model):
+    url = models.URLField()
+    name = models.CharField(max_length=500)
